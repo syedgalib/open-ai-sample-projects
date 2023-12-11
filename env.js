ff726699070db8146b928f66dbfaf11b55cd69aa
Syed Galib Ahmed <syedgalibahmed@gmail.com>
2023-12-11 18:09:25 +0600

    improved

--- env.js
+++ env.js
@@ -1,6 +1,6 @@
 export const process = {
     env: {
-        OPENAI_API_KEY: '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',
+        OPENAI_API_KEY: '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',
         CHAT_BOOT_DATABASE_CONFIG: { 
             databaseURL: 'https://open-ai-sample-chatboot-default-rtdb.firebaseio.com/'
         },
